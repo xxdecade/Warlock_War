de764b601005b63a73b8eac37c18c87906753320
Trevor Ji <xxdecade@163.com>
2023-11-04 15:45:45 +0800

    finish menu scene

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -52,38 +52,3 @@ class GameMenu {
         this.$menu.hide();
     }
 }
-class GamePlayground {
-    constructor(root) {
-        this.root = root;
-        this.$playground = $(`<div>游戏界面</div>`);
-
-        this.hide();
-        this.root.$game.append(this.$playground);
-
-        this.start();
-    }
-
-    start(){
-    }
-
-    show() {
-        this.$playground.show();
-    }
-
-    hide() {
-        this.$playground.hide();
-    }
-}
-class Game {
-    constructor(id) {
-        this.id = id;
-        this.$game = $('#' + id);
-        this.menu = new GameMenu(this);
-        this.playground = new GamePlayground(this);
-
-        this.start();
-    }
-
-    start(){
-    }
-}
