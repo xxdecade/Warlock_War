81c0f873377dd8f67dce3278a47364ef15ef3d12
jyh <xxdecade@163.com>
2023-12-03 23:23:00 +0800

    update show skill info

--- game/static/js/src/playground/notice_board/zbase.js
+++ game/static/js/src/playground/notice_board/zbase.js
@@ -5,6 +5,7 @@ class NoticeBoard extends GameObject {
         this.playground = playground;
         this.ctx = this.playground.game_map.ctx;
         this.text = "已就绪：0人";
+        this.info = "Q:选中火球\nF:选中闪现\n点击鼠标左键释放\n刷新界面可退出";
     }
 
     start() {
@@ -22,7 +23,19 @@ class NoticeBoard extends GameObject {
         this.ctx.font = "20px serif";
         this.ctx.fillStyle = "white";
         this.ctx.textAlign = "center";
+
         this.ctx.fillText(this.text, this.playground.width / 2, 20);
+
+        const infoLines = this.info.split('\n');
+        const lineHeight = 14;
+
+        this.ctx.font = "12px serif";
+        this.ctx.fillStyle = "yellow";
+        infoLines.forEach((line, index) => {
+            this.ctx.fillText(line, this.playground.width / 16, lineHeight * (index + 1));
+        });
     }
+
+
 }
 
